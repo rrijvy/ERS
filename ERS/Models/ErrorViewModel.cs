d19fbcbe700b6a9c5c290e609d4067e122c16835
rrijvy <32623510+rrijvy@users.noreply.github.com>
2019-04-18 12:10:29 +0600

    Add project files.

---
+++ ERS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ERS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
